65ffdfd9576f3bb3b2cee81f1e62899e5333e0c1
AlejandriaRamai <vyasadev931@gmail.com>
2022-06-14 09:46:48 +0200

    additional exercises

--- src/Inheritance/exercises/Integers.java
+++ src/Inheritance/exercises/Integers.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Integers {
     protected List<Integer> array;
+
     public Integers (List<Integer> array) {this.array = array; }
 
     public int getElement(Integer index) {return array.get(index); }
@@ -33,6 +34,7 @@ public class Integers {
     public void addElement(Integer index, Integer number) {
         array.set(index,number);
     }
+    public void addElement(Integer number) {array.add(number);}
 
     public void removeElement(Integer number) {
         array.remove(number);
@@ -44,7 +46,13 @@ public class Integers {
 
     public static void main(String[] args) {
 
-        Integers it = new Integers (Arrays.asList(3,5,8,10));
-        it.append(9);
+        List<Integer> vector = new ArrayList<Integer>(Arrays.asList(3,5,8,10));
+        Integers it = new SortedIntegers (vector);
+        it.append(11);
+        it.append(7);
+        System.out.println(it.getArray());
+        it.addElement(6);
+        System.out.println(it.getArray());
     }
+
 }
--- src/Inheritance/exercises/SortedIntegers.java
+++ src/Inheritance/exercises/SortedIntegers.java
@@ -10,7 +10,6 @@ public class SortedIntegers extends Integers {
 
     @Override
     public void append(Integer in) {
-        super.append(in);
         if (in > array.get(array.size()-1)) array.add(in);
         else System.out.println("Invalid value");
     }
@@ -18,27 +17,32 @@ public class SortedIntegers extends Integers {
     @Override
     public void addElement(Integer index, Integer input) {
         if (input < array.get(index+1) && input > array.get(index-1)) {
-            array.add(index, input);
+            array.set(index, input);
         }
         else {
             System.out.println("Invalid index, try again");
         }
     }
 
+    @Override
     public void addElement (Integer number) {
-
         for (int k = 0; k < array.size(); k++) {
             if (k == 0) {
-                if (number < array.get(k)) array.add(k, number);
+                if (number < array.get(k)) {
+                    array.add(k, number);
+                    break;
+                }
             }
             else {
                 if (k < array.size() - 1) {
                     if (number < array.get(k + 1) && number > array.get(k - 1)) {
                         array.add(k + 1, number);
+                        break;
                     }
                 } else {
                     if (number > array.get(k))
                         array.add(number);
+                        break;
                 }
             }
         }
